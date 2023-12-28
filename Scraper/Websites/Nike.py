ca27192666cf7135e45056ca975462a5e0fdc0d2
shreyash-01 <singhshreyash0075@gmail.com>
2023-12-29 00:05:53 +0530

    Created Nike Scraper

---
+++ Scraper/Websites/Nike.py
@@ -0,0 +1,27 @@
+import datetime
+from requests_html import HTMLSession
+from Product import Product
+import re
+
+
+class NikeScraper:
+    def __init__(self):
+        self.session = HTMLSession()
+        self.website = "Nike"
+
+    def scrape(self, url):
+        res = self.session.get(url)
+        name = res.html.find('.headline-2.css-16cqcdq', first=True)
+        price = res.html.find('.product-price.is--current-price.css-s56yt7.css-xq7tty', first=True)
+        if price is None:
+            price = res.html.find('.product-price.css-11s12ax.is--current-price.css-tpaepq', first=True)
+        url = url
+        imageurl = res.html.find('img.css-viwop1.css-m5dkrx', first=True)
+        time = datetime.datetime.now().strftime("%H:%M:%S, %d %B %Y - %A")
+
+        return Product(name.text, self.parse_price(price), url, imageurl.attrs.get('src'), time, self.website)
+
+    def parse_price(self, price):
+        return int("".join(re.findall("[0-9]", price.text))[:-2])
+
+
