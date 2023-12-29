026f6277b3124d2e57b57662cdb49bc2f54bd154
shreyash-01 <singhshreyash0075@gmail.com>
2023-12-29 08:59:31 +0530

    Created Snitch Scraper

---
+++ Scraper/Websites/Snitch.py
@@ -0,0 +1,25 @@
+import datetime
+from requests_html import HTMLSession
+from Product import Product
+import re
+
+
+class SnitchScraper:
+    def __init__(self):
+        self.session = HTMLSession()
+        self.website = "Snitch"
+
+    def scrape(self, url):
+        res = self.session.get(url)
+        name = res.html.find('.h2.product-single__title', first=True)
+        price = res.html.find('.product__price', first=True)
+        url = url
+        imageurl = res.html.find('.photoswipe__image.image-element', first=True)
+        time = datetime.datetime.now().strftime("%H:%M:%S, %d %B %Y - %A")
+
+        return Product(name.text.split('\n')[0], self.parse_price(price.text), url, "https:"+imageurl.attrs.get('src'), time, self.website)
+
+    def parse_price(self, price: str):
+        return int("".join(re.findall("[0-9]", price)))
+
+
