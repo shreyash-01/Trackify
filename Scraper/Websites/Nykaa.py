1d33f8f13bf413e2056816de3dd9974bab78a63b
shreyash-01 <singhshreyash0075@gmail.com>
2023-12-29 08:59:14 +0530

    Created Nykaa Scraper

---
+++ Scraper/Websites/Nykaa.py
@@ -0,0 +1,25 @@
+import datetime
+from requests_html import HTMLSession
+from Product import Product
+import re
+
+
+class NykaaScraper:
+    def __init__(self):
+        self.session = HTMLSession()
+        self.website = "Nykaa"
+
+    def scrape(self, url):
+        res = self.session.get(url)
+        name = res.html.find('h1', first=True)
+        price = res.html.find('.css-1jczs19', first=True)
+        url = url
+        imageurl = res.html.find('img[alt="product-thumbnail"]', first=True)
+        time = datetime.datetime.now().strftime("%H:%M:%S, %d %B %Y - %A")
+
+        return Product(name.text.split('\n')[0], self.parse_price(price.text), url, imageurl.attrs.get('src'), time, self.website)
+
+    def parse_price(self, price: str):
+        return int("".join(re.findall("[0-9]", price)))
+
+
